4f9a08d0a4f6784a98cada94e074dce297efd508
ItisReaper2 <aleshin.grigory@mail.ru>
2023-05-17 21:26:00 +0700

    menu

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1.Ввести А");
+            Console.WriteLine("2.Ввести В");
+            Console.WriteLine("3.Выполнить операцию “+”");
+            Console.WriteLine("4.выполнить операцию “-”");
+            Console.WriteLine("5.выполнить операцию “*”");
+            Console.WriteLine("6.выполнить операцию “/”");
+
+            int A = 0, B = 0, vibor = 0;
+            bool sost = true;
+
+        }
+    }
+}
