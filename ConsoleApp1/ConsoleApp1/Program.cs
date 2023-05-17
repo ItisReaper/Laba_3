a19306009d53ec3e434d8a8b984a473a3c1acef1
ItisReaper2 <aleshin.grigory@mail.ru>
2023-05-17 21:33:11 +0700

    cs 1-6

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -21,6 +21,40 @@ namespace lab_2
             int A = 0, B = 0, vibor = 0;
             bool sost = true;
 
+            Console.Write("Введите номер команды - ");
+            vibor = int.Parse(Console.ReadLine());
+
+            do
+            {
+                switch (vibor)
+                {
+                    case 1:
+                        A = int.Parse(Console.ReadLine());
+                        break;
+                    case 2:
+                        B = int.Parse(Console.ReadLine());
+                        break;
+                    case 3:
+                        A = A + B;
+                        break;
+                    case 4:
+                        A = A - B;
+                        break;
+                    case 5:
+                        A = A * B;
+                        break;
+                    case 6:
+                        if (B == 0)
+                            Console.WriteLine("Число B ровно 0");
+                        else
+                            A = A / B;
+                        break;
+                    default:
+                        sost = false;
+                        break;
+                }
+            }
+            while (true);
         }
     }
 }
